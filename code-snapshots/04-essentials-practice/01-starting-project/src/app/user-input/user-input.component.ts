80986533108fc2095144bfc86c68bc15224b199f
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-16 11:07:59 -0400

    start of the angular courses

---
+++ code-snapshots/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  calculate = output<InvestmentInput>();
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    })
+    this.enteredInitialInvestment.set('0')
+    this.enteredAnnualInvestment = signal('0');
+    this.enteredExpectedReturn = signal('0');
+    this.enteredDuration = signal('0');
+  }
+}
