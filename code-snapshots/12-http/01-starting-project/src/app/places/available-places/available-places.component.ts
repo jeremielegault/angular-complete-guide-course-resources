edb1f5a5077a4ed01e3b08b1f601afa2de02091f
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-17 11:12:00 -0400

    move requests into a service

--- code-snapshots/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ code-snapshots/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -3,8 +3,7 @@ import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { HttpClient } from '@angular/common/http';
-import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -19,25 +18,14 @@ export class AvailablePlacesComponent implements OnInit {
   isFetching = signal(false)
   error = signal('')
 
-  private httpClient = inject(HttpClient)
+  private placesService = inject(PlacesService)
   private destroyRef = inject(DestroyRef)
+
   ngOnInit() {
     this.isFetching.set(true);
 
-    const subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(
-        map((resData) => resData.places),
-        catchError((error) => {
-          console.error('HTTP error:', error);
-          return throwError(() =>
-            new Error(
-              'Something went wrong fetching the available places. Please try again later.'
-            )
-          );
-        })
-      )
-      .subscribe({
+    const subscription =
+      this.placesService.loadAvailablePlaces().subscribe({
         next: (places) => {
           console.log('places HTTP', places);
           this.places.set(places);
@@ -57,12 +45,14 @@ export class AvailablePlacesComponent implements OnInit {
     });
   }
   onSelectPlace(selectedPlace: Place) {
-    this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: selectedPlace.id
-    }).subscribe({
-      next: (resData) => {
-        console.log('selectPlace HTTP', resData);
-      }
-    })
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace.id)
+      .subscribe({
+        next: (resData) => {
+          console.log('selectPlace HTTP', resData);
+        }
+      })
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
--- code-snapshots/12-http/01-starting-project/src/app/places/user-places/user-places.component.ts
+++ code-snapshots/12-http/01-starting-project/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +12,34 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false)
+  error = signal('')
+
+  private destroyRef = inject(DestroyRef)
+  private placesService = inject(PlacesService)
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      next: (places) => {
+        console.log('places HTTP', places);
+        this.places.set(places);
+      },
+      error: (error) => {
+        console.error(error);
+        this.error.set(error.message || 'Unknown error');
+        this.isFetching.set(false);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
