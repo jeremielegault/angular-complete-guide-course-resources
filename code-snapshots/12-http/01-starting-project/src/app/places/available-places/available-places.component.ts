621ec663606fcd8a0eaa92157f4f984f548ddbb6
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-17 10:40:16 -0400

    add some http request code

--- code-snapshots/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ code-snapshots/12-http/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit, DestroyRef } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -10,7 +12,57 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
+
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false)
+  error = signal('')
+
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.error('HTTP error:', error);
+          return throwError(() =>
+            new Error(
+              'Something went wrong fetching the available places. Please try again later.'
+            )
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          console.log('places HTTP', places);
+          this.places.set(places);
+        },
+        error: (error) => {
+          console.error(error);
+          this.error.set(error.message || 'Unknown error');
+          this.isFetching.set(false);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {
+      placeId: selectedPlace.id
+    }).subscribe({
+      next: (resData) => {
+        console.log('selectPlace HTTP', resData);
+      }
+    })
+  }
 }
