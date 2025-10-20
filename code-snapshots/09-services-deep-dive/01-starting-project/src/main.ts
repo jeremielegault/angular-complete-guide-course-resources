eba38f96ec37c6a05ee64061bbefac77c30fc70b
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-20 13:35:33 -0400

    alternate way to inject services app wide from main ts

--- code-snapshots/09-services-deep-dive/01-starting-project/src/main.ts
+++ code-snapshots/09-services-deep-dive/01-starting-project/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-
+// Provides this service by default, less efficient
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService]
+// }).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
