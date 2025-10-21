60e6f227735abd1990026d7274082c5a2aafa33b
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-21 08:37:17 -0400

    initialize reactive approach to forms

--- code-snapshots/13-forms/05-reactive-forms-starting-project/src/app/auth/login/login.component.ts
+++ code-snapshots/13-forms/05-reactive-forms-starting-project/src/app/auth/login/login.component.ts
@@ -1,9 +1,21 @@
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.form.value.email, this.form.value.password)
+
+  }
+}
