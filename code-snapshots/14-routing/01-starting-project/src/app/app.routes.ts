07c1108a91a5264ce2d4d7b27e8a0a1077dffba3
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-23 10:18:12 -0400

    add dynamic page titles

--- code-snapshots/14-routing/01-starting-project/src/app/app.routes.ts
+++ code-snapshots/14-routing/01-starting-project/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveUserName, UserTasksComponent, resolveTitle } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes } from "./users/users.routes";
+
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+    title: 'No task selected'
   },
   {
     path: 'users/:userId', // your-domain/users/<uid>
@@ -17,7 +19,8 @@ export const routes: Routes = [
     },
     resolve: {
       userName: resolveUserName
-    }
+    },
+    title: resolveTitle
   },
   {
     path: '**',
