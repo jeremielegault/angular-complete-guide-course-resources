2262e3ff3d587bd1298c7e893af5ac736ecf5431
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-24 09:26:32 -0400

    pvservable and signal used together

--- code-snapshots/11-rxjs/01-starting-project/src/app/app.component.ts
+++ code-snapshots/11-rxjs/01-starting-project/src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0)
   private destroyRef = inject(DestroyRef)
+
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+    })
+  }
+
   ngOnInit(): void {
-    const subscription = interval(1000).subscribe({
+    const subscription = interval(1000).pipe(
+      map((val) => val * 2)
+    ).subscribe({
       next: (val) => console.log(val)
     });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe()
     })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
   }
 }
